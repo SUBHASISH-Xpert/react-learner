0627b020ea0f4e16143eddaf3b2ac97d898abc8a
SUBHASISH-Xpert <subhasishsahoo143s@gmail.com>
2026-03-03 22:34:05 +0530

    react component

---
+++ component_react/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="card">
+        <h1>This is react component</h1>
+        <p>This is a sample paragraph inside the Navbar component.</p>
+    </div>
+  )
+}
+
+export default Navbar
